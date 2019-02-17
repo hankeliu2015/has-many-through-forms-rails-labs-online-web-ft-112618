4cf8bb70054bac0a7208d116c0226f8051358e74
ghankeliu2015 <hanke.liu@gmail.com>
2019-02-17 17:32:12 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
+        #binding.pry
     comment = Comment.create(comment_params)
+    #@post.comments << @comment
     redirect_to comment.post
+    #redirect_to post_path(comment.post)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    #binding.pry
   end
 end
